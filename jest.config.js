07004a1500b02f4832b5b1f999f09e51e860aa2e
Ritesh Tarway <rtarway@gmail.com>
2022-08-24 04:00:24 -0500

    modified path for global set up and tear down

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,6 @@ module.exports = {
         "/node_modules/"
       ],
       "verbose": true,
-      "globalSetup": '/Users/riteshtarway/visual_code_workspace/nodejs_workspace/inventory_service/test-setup-globals.js',
-      "globalTeardown": '/Users/riteshtarway/visual_code_workspace/nodejs_workspace/inventory_service/test-teardown-globals.js',
+      "globalSetup": './test-setup-globals.js',
+      "globalTeardown": './test-teardown-globals.js',
 };
